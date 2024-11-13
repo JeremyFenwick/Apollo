c12aa4cdafe2615342ff2b63705809a291ec07d8
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2024-11-13 20:03:17 +1100

    Added ring pattern

---
+++ Apollo.Core/Geometry/Patterns/Ring.cs
@@ -0,0 +1,25 @@
+﻿using Apollo.Display.AbstractClasses;
+using Apollo.Geometry.Interfaces;
+using Apollo.Math;
+using Apollo.Math.AbstractClasses;
+
+namespace Apollo.Geometry.Patterns;
+
+public class Ring : IPattern
+{
+    public Matrix Transform { get; set; }
+    private readonly AbstractColour _c1;
+    private readonly AbstractColour _c2;
+    
+    public Ring(AbstractColour c1, AbstractColour c2)
+    {
+        (this._c1, this._c2) = (c1, c2);
+        Transform = Matrix.Identity();
+    }
+    
+    public AbstractColour ColourAt(AbstractTuple point)
+    {
+        var temp = System.Math.Sqrt(System.Math.Pow(point.X, 2) + System.Math.Pow(point.Z, 2));
+        return System.Math.Floor(temp) % 2 == 0 ? _c1 : _c2;
+    }
+}
---
+++ Apollo.Tests/Geometry/RingTests.cs
@@ -0,0 +1,18 @@
+﻿using Apollo.Display.ColourPresets;
+using Apollo.Geometry.Patterns;
+using Apollo.Math;
+
+namespace Apollo.Tests.Geometry;
+
+public class RingTests
+{
+    [Test]
+    public void RingTest()
+    {
+        var ring = new Ring(new White(), new Black());
+        Assert.That(ring.ColourAt(new Point(0, 0, 0)) == new White());
+        Assert.That(ring.ColourAt(new Point(1, 0, 0)) == new Black());
+        Assert.That(ring.ColourAt(new Point(0, 0, 1)) == new Black());
+        Assert.That(ring.ColourAt(new Point(0.708, 0, 0.708)) == new Black());
+    }
+}
